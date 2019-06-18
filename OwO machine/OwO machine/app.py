509d09719b2564b637a6f5522abb3987fbd0ac53
Daniel <dandeng24@live.com>
2019-06-19 08:52:16 +1200

    Add files via upload
    I made it in 1 hour with a friend

---
+++ OwO machine/OwO machine/app.py
@@ -0,0 +1,56 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask, render_template, request
+import random
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+#special thanks to https://github.com/Zenrac/TextToOwO I may have stolen t source code.
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+def last_replace(s, old, new):
+    li = s.rsplit(old, 1)
+    return new.join(li)
+
+def text_to_owo(text):
+    """ Converts your text to OwO """
+    smileys = [';;w;;', '^w^', '>w<', 'UwU', '(・`ω\´・)', '(´・ω・\`)']
+
+    text = text.replace('L', 'W').replace('l', 'w')
+    text = text.replace('R', 'W').replace('r', 'w')
+
+    text = last_replace(text, '!', '! {}'.format(random.choice(smileys)))
+    text = last_replace(text, '?', '? owo')
+    text = last_replace(text, '.', '. {}'.format(random.choice(smileys)))
+
+    for v in vowels:
+        if 'n{}'.format(v) in text:
+            text = text.replace('n{}'.format(v), 'ny{}'.format(v))
+        if 'N{}'.format(v) in text:
+            text = text.replace('N{}'.format(v), 'N{}{}'.format('Y' if v.isupper() else 'y', v))
+
+    return text
+
+@app.route('/', methods = ["POST", "GET"])
+def hello():
+	text = ""
+	if request.method == "POST":
+		tester = request.form['tester']
+		text = text_to_owo(tester)
+	return render_template("index.html", text = text)
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, debug = True)
+
